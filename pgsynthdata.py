b36471ea911116bd420300d378f317892e5853c3
labiangashi <labiangashi@gmail.com>
2020-04-27 01:22:35 +0200

    Adding pg_stats and some refactoring

--- pgsynthdata.py
+++ pgsynthdata.py
@@ -5,20 +5,24 @@ import random
 import subprocess
 import sys
 from subprocess import Popen
+from typing import Dict
 
 import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-import random_helper
-import sql_helper
+import postgres
+import utils
 
 
 class DatabaseError(Exception):
     """Raised when any error happens in the database"""
 
 
-tables_and_columns = dict()
+START_DATE = datetime.date(year=1950, month=1, day=1)
+END_DATE = datetime.date.today()
+
+table_information: Dict = {}
 
 __version__ = '1.0'
 examples = '''How to use pgsynthdata.py:
@@ -55,12 +59,12 @@ def main():
     parameters = f'{db_name}{host}{port}{user}{password}'
 
     if args.show:
-        connect_show(args)
+        show(args)
     else:
         if args.DBNAMEGEN is None:
             sys.exit('When "-generate" argument is given, the following argument is required: DBNAMEGEN')
         else:
-            connect_gen(args, args.DBNAMEIN, args.DBNAMEGEN, args.owner)
+            generate(args, args.DBNAMEIN, args.DBNAMEGEN, args.owner)
 
 
 def parse_arguments():
@@ -86,27 +90,26 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def connect_show(args):
+def show(args):
     connection = None
     try:
-        connection = sql_helper.db_connect(args.DBNAMEIN, args.user, args.hostname, args.port, args.password)
+        connection = postgres.db_connect(args.DBNAMEIN, args.user, args.hostname, args.port, args.password)
     except psycopg2.DatabaseError:
         sys.exit('''Connection failed because of at least one of the following reasons:
             Database does not exist
             User does not exist
             Wrong password''')
 
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = connection.cursor()
 
-    sql_helper.show_database_stats(cursor)
+    postgres.show_database_stats(cursor)
     cursor.close()
 
 
-def connect_gen(args, db_name_in, db_name_gen, owner_name=None):
+def generate(args, db_name_in, db_name_gen, owner_name=None):
     connection = None
     try:
-        connection = sql_helper.db_connect(db_name_in, args.user, args.hostname, args.port, args.password)
+        connection = postgres.db_connect(db_name_in, args.user, args.hostname, args.port, args.password)
     except psycopg2.DatabaseError:
         sys.exit('''Connection failed because of at least one of the following reasons:
             Database does not exist
@@ -116,100 +119,97 @@ def connect_gen(args, db_name_in, db_name_gen, owner_name=None):
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = connection.cursor()
 
-    sql_helper.create_database(connection, cursor, db_name_gen, owner_name)
-
-    try:
-        connection = sql_helper.db_connect(db_name_gen, args.user, args.hostname, args.port, args.password)
-    except psycopg2.DatabaseError:
-        sys.exit(f"Could not connect to the newly created database: {args.DBNAMEGEN}")
-
-    cursor = connection.cursor()
+    postgres.create_database(connection, cursor, db_name_gen, owner_name)
 
     copy_database_structure(args)
 
-    try:
-        cursor.execute("""
-        SELECT 
-            nspname AS schemaname,relname as tablename, reltuples::bigint as rowcount,rank() over(order by reltuples desc)
-        FROM pg_class C
-        LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
-        WHERE 
-            nspname NOT IN ('pg_catalog', 'information_schema') AND
-            relkind='r' 
-        ORDER BY tablename;""")
-    except psycopg2.DatabaseError:
-        sys.exit('Could not retrieve the generated database\'s table information')
+    table_results = postgres.get_table_information(cursor, generated=True)
 
-    table_results = cursor.fetchall()
-
+    insert_dict = dict()
     for table_entry in table_results:
+        cursor = connection.cursor()
         table_name = table_entry[1]
-        cursor.execute(f"""
-            SELECT a.attname
-            FROM   pg_index i
-            JOIN   pg_attribute a ON a.attrelid = i.indrelid
-                                 AND a.attnum = ANY(i.indkey)
-            WHERE  i.indrelid = '{table_name}'::regclass
-            AND    i.indisprimary;""")
 
-        primary_column_result = cursor.fetchone()
-
+        primary_column_result = postgres.get_table_primary_key(cursor, table_name, generated=True)
         primary_column = None
+
         if primary_column_result:
             primary_column = primary_column_result[0]
 
-        try:
-            cursor.execute(f"""
-                SELECT 
-                    column_name, data_type, character_maximum_length
-                FROM   information_schema.columns
-                WHERE  table_name = '{table_name}'
-                ORDER  BY ordinal_position;
-                """)
-        except psycopg2.DatabaseError:
-            sys.exit(f'Could not get columns for the generated "{table_name}" table')
+        column_results = postgres.get_column_information(cursor, table_name, generated=True)
 
-        column_results = cursor.fetchall()
-        columns_tuple = list()
+        table_information[table_name] = {}
+        table_information[table_name]["column_information"] = {}
+        table_information[table_name]["stats"] = {}
 
         for column_entry in column_results:
             if column_entry[0] != primary_column:
-                columns_tuple.append((column_entry[0], column_entry[1], column_entry[2]))
+                columns_dict = dict()
+                columns_dict["column_name"] = column_entry[0]
+                columns_dict["data_type"] = column_entry[1]
+                columns_dict["max_length"] = column_entry[2]
+                table_information[table_name]["column_information"][column_entry[0]] = columns_dict
 
-        tables_and_columns[table_name] = columns_tuple
+        table_stats = postgres.get_table_stats(cursor, table_name)
 
-        columns_information = list()
+        for stats_entry in table_stats:
+            stats_dict = dict()
+            stats_dict["column_name"] = stats_entry[0]
+            stats_dict["null_frac"] = stats_entry[1]
+            stats_dict["avg_width"] = stats_entry[2]
+            stats_dict["n_distinct"] = stats_entry[3]
+            stats_dict["most_common_vals"] = stats_entry[4]
+            stats_dict["most_common_freqs"] = stats_entry[5]
+            stats_dict["histogram_bounds"] = stats_entry[6]
+            stats_dict["correlation"] = stats_entry[7]
+            table_information[table_name]["stats"][stats_entry[0]] = stats_dict
 
-        for column_info in tables_and_columns.get(table_name):
-            columns_information.append(column_info[0])
+        column_names = list()
 
-        start_date = datetime.date(year=1950, month=1, day=1)
-        end_date = datetime.date(year=2020, month=1, day=1)
+        for column_name in table_information.get(table_name)["column_information"].keys():
+            column_names.append(column_name)
 
+        insert_query = ""
         for lp in range(100):
-            insert_query = "INSERT INTO {}("
-            insert_query += '{0}{1}'.format(', '.join(columns_information), ') VALUES (')
+            insert_query += "INSERT INTO {table_name}("
+            insert_query += '{0}{1}'.format(', '.join(column_names), ') VALUES (')
 
             column_values = list()
 
-            for column_info in tables_and_columns.get(table_name):
-                if column_info[1] == 'integer' or column_info[1] == 'numeric':
-                    column_values.append("{0}".format(random_helper.random_number(0, 1000)))
-                elif column_info[1] == 'date':
-                    column_values.append("'{0}'".format(random_helper.random_date(start_date, end_date)))
+            for column_info in table_information[table_name]["column_information"].values():
+                data_type = column_info.get("data_type")
+                max_length = column_info.get("max_length")
+
+                if data_type == 'integer' or data_type == 'numeric':
+                    column_values.append("{0}".format(utils.random_number(0, 1000)))
+                elif data_type == 'date':
+                    column_values.append("'{0}'".format(utils.random_date(START_DATE, END_DATE)))
                 else:
-                    column_values.append("'{0}'".format(random_helper.random_word(
-                        random.randrange(50 if column_info[2] is None else (column_info[2] + 1))
+                    column_values.append("'{0}'".format(utils.random_word(
+                        random.randrange(50 if max_length is None else (max_length + 1))
                     )))
 
-            insert_query += '{0}{1}'.format(', '.join(column_values), ');')
+            insert_query += '{0}{1}'.format(', '.join(column_values), ');\n')
 
-            cursor.execute(
-                sql.SQL(insert_query).format(
-                    sql.Identifier(table_name)
-                )
+        insert_dict[table_name] = insert_query
+
+    try:
+        connection = postgres.db_connect(db_name_gen, args.user, args.hostname, args.port, args.password)
+    except psycopg2.DatabaseError:
+        sys.exit(f"Could not connect to the newly created database: {args.DBNAMEGEN}")
+
+    # with open('description.json', 'w') as file:
+    #   file.write(json.dumps(table_information, indent=4))
+
+    cursor = connection.cursor()
+    for table_name, insert_query in insert_dict.items():
+        cursor.execute(
+            sql.SQL(insert_query).format(
+                table_name=sql.Identifier(table_name)
             )
+        )
 
+    sys.stdout.write(f"Successfully generated data into the new \"{db_name_gen}\" database.")
     connection.commit()
 
     cursor.close()
